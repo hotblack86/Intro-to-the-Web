0dee2659f05554a53fb7a5aa290ac15f0bac0fe3
James Hotblack <james.c.hotblack@gmail.com>
2018-12-10 17:53:59 +0000

    Sinatra: Introducing params

--- app.rb
+++ app.rb
@@ -16,8 +16,15 @@ get '/cow' do
     "Moo"
 end
 
-get '/cat' do
+get '/random-cat' do
    @random_name = ["Amigo", "Oscar", "Viking"].sample
    erb(:index)
 
 end
+
+get '/named-cat' do
+    p params[:name]
+    @random_name = params[:name]
+    erb(:index)
+ 
+ end
